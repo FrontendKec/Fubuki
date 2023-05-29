d7cda059ce5e599652858d0c1c54dca0749dfe11
FrontendKec <kamal.ennour.chehami@gmail.com>
2023-05-29 20:43:56 +0100

    updated database

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,12 +7,12 @@ const db_schema = new mongoose.Schema({ anime: String, character: String, quote:
 const db_model = mongoose.model('Quote', db_schema);
 
 router.get('/', (ctx) => {
-  ctx.body = { 'API': 'Welcome to Fubuki 🎉' };
+  ctx.body = 'Welcome to Fubuki 🎉'
 });
 
 router.get('/api/quotes', async (ctx) => {
   const quotes = await db_model.find();
-  ctx.body = { 'Available Quotes': quotes.length + ' Quote' };
+  ctx.body = 'Available Quotes: ' + quotes.length + ' Quote 😊'
 });
 
 router.get('/api/anime/all', async (ctx) => {
@@ -23,13 +23,13 @@ router.get('/api/anime/all', async (ctx) => {
       list.push(quote.anime);
     }
   });
-  ctx.body = [list, 'Count: ' + list.length];
+  ctx.body = [{ 'Count': list.length + ' Anime'}, list];
 });
 
 router.get('/api/quote/random', async (ctx) => {
   const quotes = await db_model.find({}, { _id: 0 });
   const random = quotes[Math.floor(Math.random() * quotes.length)];
-  ctx.body = random;
+  ctx.body = random
 });
 
 router.get('/api/quotes/random', async (ctx) => {
@@ -39,7 +39,7 @@ router.get('/api/quotes/random', async (ctx) => {
     const random = Math.floor(Math.random() * quotes.length);
     list.push(quotes[random]);
   }
-  ctx.body = list;
+  ctx.body = list
 });
 
 export default router;
